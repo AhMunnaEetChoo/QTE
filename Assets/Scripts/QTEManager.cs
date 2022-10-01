b87cf23f94de732fdcb64531cafb991c7f38fc49
Ollie <38665165+AhMunnaEetChoo@users.noreply.github.com>
2022-10-01 18:51:59 +0100

    Basic QTE logic

--- Assets/Scripts/QTEManager.cs
+++ Assets/Scripts/QTEManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine;
 using UnityEngine.Video;
 using TMPro;
@@ -10,12 +11,27 @@ public class QTEManager : MonoBehaviour
     private StateMachine m_stateMachine = new StateMachine();
     public VideoPlayer m_videoPlayer;
     public TMP_Text m_timerText;
+    public GameObject m_qtePrefab;
+    public GameObject m_resultPrefab;
+
+    [System.Serializable]
+    public class QTE
+    {
+        public string m_text;
+        public string m_button;
+        public Vector2 m_screenPos;
+        public float m_promptTime;
+        public float m_triggerTime;
+        public float m_perfectBuffer;
+        public float m_greatBuffer;
+        public float m_coolBuffer;
+    };
 
     [System.Serializable]
     public class ClipData
     {
         public string m_url;
-        public string m_text;
+        public List<QTE> m_qtes = new List<QTE>();
     };
     public List<ClipData> m_clipData = new List<ClipData>();
 
@@ -42,34 +58,119 @@ public class QTEManager : MonoBehaviour
 
     public class PlayClip : IState
     {
+        private QTEManager m_manager;
         private List<ClipData> m_clipDataList;
         private VideoPlayer m_videoPlayer;
+        private int m_clipIndex = 0;
 
-        public PlayClip(List<ClipData> _dataList, VideoPlayer _videoPlayer)
+        enum QTEStage
         {
+            Initialising,
+            ShowPrompt,
+            KeyPressed
+        }
+        private QTEStage m_qteStage = QTEStage.Initialising;
+        private ClipData m_currentClipData;
+        private int m_currentQTE = 0;
+        private GameObject m_currentPrompt;
+        private GameObject m_currentResult;
+
+        public PlayClip(QTEManager _manager, List<ClipData> _dataList, VideoPlayer _videoPlayer)
+        {
+            m_manager = _manager;
             m_clipDataList = _dataList;
             m_videoPlayer = _videoPlayer;
             m_videoPlayer.loopPointReached += EndReached;
         }
         void EndReached(UnityEngine.Video.VideoPlayer vp)
         {
+            // TODO: is final video ...
+
+            OnExit();
             OnEnter();
         }
 
         public void Tick()
         {
+            if (m_currentQTE >= m_currentClipData.m_qtes.Count)
+            {
+                return;
+            }
+            QTE currentQTE = m_currentClipData.m_qtes[m_currentQTE];
+
+            switch (m_qteStage)
+            {
+                case QTEStage.Initialising:
+                    if(m_videoPlayer.time > currentQTE.m_promptTime)
+                    {
+                        GameObject canvas = GameObject.Find("Canvas");
+
+                        // spawn the text
+                        m_currentPrompt = Instantiate(m_manager.m_qtePrefab, currentQTE.m_screenPos, Quaternion.identity, canvas.transform);
+                        TMP_Text newText = m_currentPrompt.GetComponent<TMP_Text>();
+                        newText.text = currentQTE.m_text;
+                        m_qteStage = QTEStage.ShowPrompt;
+                    }
+                    break;
+                case QTEStage.ShowPrompt:
+                    // if player presses the right QTE button
+                    if (((KeyControl)Keyboard.current[currentQTE.m_button]).isPressed)
+                    {
+                        // now check against the correct time..
+                        float difference = Mathf.Abs(currentQTE.m_triggerTime - (float)m_videoPlayer.time);
+                        string resultText = "";
+                        if (difference < currentQTE.m_perfectBuffer)
+                        {
+                            resultText = "PERFECT!";
+                        }
+                        else if(difference < currentQTE.m_greatBuffer)
+                        {
+                            resultText = "GREAT!";
+                        }
+                        else if(difference < currentQTE.m_coolBuffer)
+                        {
+                            resultText = "Cool";
+                        }
+                        else
+                        {
+                            resultText = "Shame";
+                            // fail
+                        }
+
+                        // for now spawn some test text
+                        GameObject canvas = GameObject.Find("Canvas");
+                        // spawn the text
+                        m_currentResult = Instantiate(m_manager.m_resultPrefab, new Vector2(300.0f, 300.0f), Quaternion.identity, canvas.transform);
+                        TMP_Text newText = m_currentResult.GetComponent<TMP_Text>();
+                        newText.text = resultText;
+                        m_qteStage = QTEStage.KeyPressed;
+
+                        // for now destroy
+                        GameObject.Destroy(m_currentPrompt);
+                    }
+                    break;
+                case QTEStage.KeyPressed:
+                    // nothing
+                    break;
+            }
         }
         public void OnEnter()
         {
             // choose a clip
-            int index = Random.Range(0, m_clipDataList.Count);
-            m_videoPlayer.url = m_clipDataList[index].m_url;
+            //int index = Random.Range(0, m_clipDataList.Count);
+            m_currentClipData = m_clipDataList[m_clipIndex];
+            m_videoPlayer.url = m_currentClipData.m_url;
             m_videoPlayer.Play();
-
-            // start timers
         }
 
-        public void OnExit() { }
+        public void OnExit()
+        {
+            GameObject.Destroy(m_currentPrompt);
+            GameObject.Destroy(m_currentResult);
+            m_currentQTE = 0;
+            m_qteStage = QTEStage.Initialising;
+            m_clipIndex++;
+        }
     }
 
     // Start is called before the first frame update
@@ -77,7 +178,7 @@ public class QTEManager : MonoBehaviour
     {
         // setup state machine
         MainMenu mainMenuState = new MainMenu();
-        PlayClip playClipState = new PlayClip(m_clipData, m_videoPlayer);
+        PlayClip playClipState = new PlayClip(this, m_clipData, m_videoPlayer);
 
         m_stateMachine.AddTransition(mainMenuState, playClipState, mainMenuState.IsComplete);
         m_stateMachine.SetState(mainMenuState);
