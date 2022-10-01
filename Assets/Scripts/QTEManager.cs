268792f5426f451df616563872e893e93943804f
SabreDance <thomascarrell@gmail.com>
2022-10-01 22:20:38 +0100

    Added a bunch of fbx objects but havent added them to the scene yet

--- Assets/Scripts/QTEManager.cs
+++ Assets/Scripts/QTEManager.cs
@@ -14,26 +14,34 @@ public class QTEManager : MonoBehaviour
     public GameObject m_qtePrefab;
     public GameObject m_resultPrefab;
 
+
     [System.Serializable]
     public class QTE
     {
-        public string m_text;
-        public string m_button;
-        public Vector2 m_screenPos;
-        public float m_promptTime;
-        public float m_triggerTime;
-        public float m_perfectBuffer;
-        public float m_greatBuffer;
-        public float m_coolBuffer;
+        public string text = "Press [f] to pay respect";
+        public string button = "f";
+        public Vector2 screenPos = new Vector2(300,350);
+        public float promptTime = 1.0f;
+        public float triggerTime = 7.0f;
+        public float perfectBuffer = 0.5f;
+        public float greatBuffer = 0.8f;
+        public float coolBuffer = 1.0f;
     };
 
     [System.Serializable]
     public class ClipData
     {
-        public string m_url;
-        public List<QTE> m_qtes = new List<QTE>();
+        public string url;
+        public List<QTE> qtes = new List<QTE>();
+        public float volume = 1.0f;
     };
-    public List<ClipData> m_clipData = new List<ClipData>();
+
+    [System.Serializable]
+    public class GameData
+    {
+        public List<ClipData> clipData = new List<ClipData>();
+    };
+    public GameData m_gameData = new GameData();
 
     public class MainMenu : IState
     {
@@ -74,6 +82,18 @@ public class QTEManager : MonoBehaviour
         private int m_currentQTE = 0;
         private GameObject m_currentPrompt;
         private GameObject m_currentResult;
+        enum QTEResult
+        {
+            None,
+            Perfect,
+            Great,
+            Cool,
+            Shame,
+            TooBad,
+            Whoops
+        }
+        private QTEResult m_qteResult = QTEResult.None;
+        private float m_qteResultTimer = 0.0f;
 
         public PlayClip(QTEManager _manager, List<ClipData> _dataList, VideoPlayer _videoPlayer)
         {
@@ -92,51 +112,58 @@ public class QTEManager : MonoBehaviour
 
         public void Tick()
         {
-            if (m_currentQTE >= m_currentClipData.m_qtes.Count)
+            if (m_currentQTE >= m_currentClipData.qtes.Count)
             {
                 return;
             }
-            QTE currentQTE = m_currentClipData.m_qtes[m_currentQTE];
+            QTE currentQTE = m_currentClipData.qtes[m_currentQTE];
 
             switch (m_qteStage)
             {
                 case QTEStage.Initialising:
-                    if(m_videoPlayer.time > currentQTE.m_promptTime)
+                    if(m_videoPlayer.time > currentQTE.promptTime)
                     {
                         GameObject canvas = GameObject.Find("Canvas");
 
                         // spawn the text
-                        m_currentPrompt = Instantiate(m_manager.m_qtePrefab, currentQTE.m_screenPos, Quaternion.identity, canvas.transform);
+                        m_currentPrompt = Instantiate(m_manager.m_qtePrefab, currentQTE.screenPos, Quaternion.identity, canvas.transform);
                         TMP_Text newText = m_currentPrompt.GetComponent<TMP_Text>();
-                        newText.text = currentQTE.m_text;
+                        newText.text = currentQTE.text;
                         m_qteStage = QTEStage.ShowPrompt;
                     }
                     break;
                 case QTEStage.ShowPrompt:
                     // if player presses the right QTE button
-                    if (((KeyControl)Keyboard.current[currentQTE.m_button]).isPressed)
+                    if (((KeyControl)Keyboard.current[currentQTE.button]).isPressed)
                     {
                         // now check against the correct time..
-                        float difference = Mathf.Abs(currentQTE.m_triggerTime - (float)m_videoPlayer.time);
+                        float difference = Mathf.Abs(currentQTE.triggerTime - (float)m_videoPlayer.time);
                         string resultText = "";
-                        if (difference < currentQTE.m_perfectBuffer)
+                        if (difference < currentQTE.perfectBuffer)
                         {
+                            FMODUnity.RuntimeManager.PlayOneShot("event:/Instruction_SuccessfulHitSound");
                             resultText = "PERFECT!";
+                            m_qteResult = QTEResult.Perfect;
                         }
-                        else if(difference < currentQTE.m_greatBuffer)
+                        else if(difference < currentQTE.greatBuffer)
                         {
+                            FMODUnity.RuntimeManager.PlayOneShot("event:/Instruction_SuccessfulHitSound");
                             resultText = "GREAT!";
+                            m_qteResult = QTEResult.Great;
                         }
-                        else if(difference < currentQTE.m_coolBuffer)
+                        else if(difference < currentQTE.coolBuffer)
                         {
+                            FMODUnity.RuntimeManager.PlayOneShot("event:/Instruction_SuccessfulHitSound");
                             resultText = "Cool";
+                            m_qteResult = QTEResult.Cool;
                         }
                         else
                         {
+                            FMODUnity.RuntimeManager.PlayOneShot("event:/Instruction_Fail");
                             resultText = "Shame";
-                            // fail
+                            m_qteResult = QTEResult.Shame;
                         }
-
+                        m_qteResultTimer = 0.0f;
                         // for now spawn some test text
                         GameObject canvas = GameObject.Find("Canvas");
                         // spawn the text
@@ -150,6 +177,27 @@ public class QTEManager : MonoBehaviour
                     }
                     break;
                 case QTEStage.KeyPressed:
+                    m_qteResultTimer += Time.deltaTime;
+                    if(m_qteResultTimer > 1.0f)
+                    {
+                        //after 1 second
+                        switch (m_qteResult)
+                        {
+                            case QTEResult.Perfect:
+                                FMODUnity.RuntimeManager.PlayOneShot("event:/Instruction_Perfect");
+                                break;
+                            case QTEResult.Great:
+                                FMODUnity.RuntimeManager.PlayOneShot("event:/Instruction_Great");
+                                break;
+                            case QTEResult.Cool:
+                                FMODUnity.RuntimeManager.PlayOneShot("event:/Instruction_Cool");
+                                break;
+                            case QTEResult.Shame:
+                                FMODUnity.RuntimeManager.PlayOneShot("event:/Instruction_Shame");
+                                break;
+                        }
+                        m_qteResult = QTEResult.None;
+                    }
                     // nothing
                     break;
             }
@@ -159,8 +207,12 @@ public class QTEManager : MonoBehaviour
             // choose a clip
             //int index = Random.Range(0, m_clipDataList.Count);
             m_currentClipData = m_clipDataList[m_clipIndex];
-            m_videoPlayer.url = m_currentClipData.m_url;
+            m_videoPlayer.url = m_currentClipData.url;
             m_videoPlayer.Play();
+            for(ushort i = 0; i < m_videoPlayer.audioTrackCount; ++i)
+            {
+                m_videoPlayer.SetDirectAudioVolume(i, m_currentClipData.volume);
+            }
         }
 
         public void OnExit()
@@ -169,6 +221,7 @@ public class QTEManager : MonoBehaviour
             GameObject.Destroy(m_currentResult);
             m_currentQTE = 0;
             m_qteStage = QTEStage.Initialising;
+            m_qteResult = QTEResult.None;
             m_clipIndex++;
         }
     }
@@ -178,7 +231,7 @@ public class QTEManager : MonoBehaviour
     {
         // setup state machine
         MainMenu mainMenuState = new MainMenu();
-        PlayClip playClipState = new PlayClip(this, m_clipData, m_videoPlayer);
+        PlayClip playClipState = new PlayClip(this, m_gameData.clipData, m_videoPlayer);
 
         m_stateMachine.AddTransition(mainMenuState, playClipState, mainMenuState.IsComplete);
         m_stateMachine.SetState(mainMenuState);
@@ -192,5 +245,10 @@ public class QTEManager : MonoBehaviour
         m_timerText.text = string.Format("{0:0}", seconds);
 
         m_stateMachine.Tick();
+    }
+
+    void InitialiseFromJSON(string _jsonString)
+    {
+        m_gameData = JsonUtility.FromJson<GameData>(_jsonString);
     }
 }
